078ba4f5d5c860604eff0c7e3bb045739faea4b0
DenizGithub <denizarslantr@gmail.com>
2026-05-18 20:08:18 +0300

    Add files via upload

--- config/firebase.js
+++ config/firebase.js
@@ -1,10 +1,10 @@
 // Piano Deri — Firebase Konfigürasyon
 const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyCowXATB4ptuUTlwmoWFGK2Rgx7O3tW-qM",
+  apiKey: "AIzaSyCoWXATB4ptduI1wm0WFGkZRgx703tW-qM",
   authDomain: "chattakip-32c31.firebaseapp.com",
   databaseURL: "https://chattakip-32c31-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "chattakip-32c31",
-  storageBucket: "chattakip-32c31.appspot.com",
+  storageBucket: "chattakip-32c31.firebasestorage.app",
   messagingSenderId: "934651992903",
   appId: "1:934651992903:web:37ddc3e05c5f9caf260a62"
 };
--- config/firebase_arac_takip.js
+++ config/firebase_arac_takip.js
@@ -1,14 +1,5 @@
-// Araç Takip için AYRI Firebase projesi konfigürasyonu
-// Firebase Console > Project settings > General > Your apps > Web app > Firebase SDK config
-// Yeni Firebase açınca burayı kendi yeni proje bilgilerinle değiştir.
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 // Araç Takip — Firebase Konfigürasyon
+// Bu dosya GitHub Pages için normal script olarak çalışır. Import satırı YOKTUR.
 const FIREBASE_ARAC_CONFIG = {
   apiKey: "AIzaSyCoWXATB4ptduI1wm0WFGkZRgx703tW-qM",
   authDomain: "chattakip-32c31.firebaseapp.com",
@@ -18,8 +9,3 @@ const FIREBASE_ARAC_CONFIG = {
   messagingSenderId: "934651992903",
   appId: "1:934651992903:web:37ddc3e05c5f9caf260a62"
 };
-
-
-// Bu sürüm Google Maps kullanmaz.
-// Harita OpenStreetMap + Leaflet ile ücretsiz çalışır.
-// TNB Web Servis bilgileri Araç Takip > Ayarlar ekranından girilir.
