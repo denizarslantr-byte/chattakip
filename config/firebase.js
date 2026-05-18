0b61b0503fd886c272bdec7898cddb7c340768e2
DenizGithub <denizarslantr@gmail.com>
2026-05-18 19:52:31 +0300

    Add files via upload

--- config/firebase.js
+++ config/firebase.js
@@ -1,12 +1,12 @@
 // Piano Deri — Firebase Konfigürasyon
 const FIREBASE_CONFIG = {
-  apiKey: "BURAYA_FIREBASE_API_KEY",
+  apiKey: "AIzaSyCowXATB4ptuUTlwmoWFGK2Rgx7O3tW-qM",
   authDomain: "chattakip-32c31.firebaseapp.com",
   databaseURL: "https://chattakip-32c31-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "chattakip-32c31",
   storageBucket: "chattakip-32c31.appspot.com",
-  messagingSenderId: "BURAYA_SENDER_ID",
-  appId: "BURAYA_APP_ID"
+  messagingSenderId: "934651992903",
+  appId: "1:934651992903:web:37ddc3e05c5f9caf260a62"
 };
 
 const ADMIN_PIN_KEY = "adminPin";
